a63407c6c07b50799efb6fc20caf2d364017f32f
andikadafap28-alt <andikadafap28@gmail.com>
2026-05-23 14:37:02 +0700

    Perbaikan Error 500 Read Only Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,32 @@
 <?php
 
-// Forward Vercel requests to normal Laravel public directory
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Tweak for Vercel Serverless Read-Only Filesystem
+// Create required directories in /tmp
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/cache/data')) {
+    mkdir('/tmp/storage/framework/cache/data', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/sessions')) {
+    mkdir('/tmp/storage/framework/sessions', 0755, true);
+}
+if (!is_dir('/tmp/storage/logs')) {
+    mkdir('/tmp/storage/logs', 0755, true);
+}
+
+// Register the Composer autoloader
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Force Laravel to use /tmp for its storage directory
+$app->useStoragePath('/tmp/storage');
+
+// Handle the Request
+$app->handleRequest(Request::capture());
