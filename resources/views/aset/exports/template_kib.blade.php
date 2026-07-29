dfea7f905d271cab0b47b3bb238bf65bb7d5b9eb
andikadafap28-alt <andikadafap28@gmail.com>
2026-07-30 00:25:29 +0700

    TANPA MERGER

--- resources/views/aset/exports/template_kib.blade.php
+++ resources/views/aset/exports/template_kib.blade.php
@@ -1,18 +1,52 @@
 <table>
     <thead>
         <tr>
-            <th style="font-weight: bold; background-color: #d1d5db;">Kode Barang</th>
-            <th style="font-weight: bold; background-color: #d1d5db;">Nama Barang</th>
-            <th style="font-weight: bold; background-color: #d1d5db;">NIBAR</th>
-            <th style="font-weight: bold; background-color: #d1d5db;">Nomor Register</th>
-            <th style="font-weight: bold; background-color: #d1d5db;">Lokasi</th>
-            <th style="font-weight: bold; background-color: #d1d5db;">Kondisi</th>
-            <th style="font-weight: bold; background-color: #d1d5db;">Tahun Perolehan</th>
-            <th style="font-weight: bold; background-color: #d1d5db;">Harga Perolehan</th>
-            <th style="font-weight: bold; background-color: #d1d5db;">Merk</th>
-            <th style="font-weight: bold; background-color: #d1d5db;">Penyedia</th>
-            <th style="font-weight: bold; background-color: #d1d5db;">Tanggal BAST</th>
-            <th style="font-weight: bold; background-color: #d1d5db;">Keterangan</th>
+            <th colspan="2">Penggolongan dan Kodefikasi Barang</th>
+            <th rowspan="2">NIBAR</th>
+            <th rowspan="2">Nomor Register</th>
+            <th rowspan="2">Spesifikasi Nama Barang</th>
+            <th rowspan="2">Spesifikasi Lainnya</th>
+            <th rowspan="2">Jumlah</th>
+            <th rowspan="2">Satuan</th>
+            <th rowspan="2">Lokasi</th>
+            <th rowspan="2">Titik Koordinat</th>
+            <th colspan="4">Bukti Kepemilikan Dokumen</th>
+            <th rowspan="2">Harga Satuan Perolehan Rp.</th>
+            <th rowspan="2">Nilai Perolehan Rp.</th>
+            <th rowspan="2">Cara Perolehan</th>
+            <th rowspan="2">Tanggal Perolehan</th>
+            <th rowspan="2">Status Penggunaan</th>
+            <th rowspan="2">Keterangan</th>
+        </tr>
+        <tr>
+            <th>Kode Barang</th>
+            <th>Nama Barang</th>
+            <th>Nama</th>
+            <th>Nomor</th>
+            <th>Tanggal</th>
+            <th>Nama Kepemilikan Dokumen</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+            <th>7</th>
+            <th>8</th>
+            <th>9</th>
+            <th>10</th>
+            <th>11</th>
+            <th>12</th>
+            <th>13</th>
+            <th>14</th>
+            <th>15</th>
+            <th>16</th>
+            <th>17</th>
+            <th>18</th>
+            <th>19</th>
+            <th>20</th>
         </tr>
     </thead>
     <tbody>
@@ -21,13 +55,21 @@
             <td>Contoh Barang</td>
             <td style="text-align: left;">120135240200000010000020171310101040060000001</td>
             <td>0001</td>
+            <td></td>
+            <td></td>
+            <td>1</td>
+            <td>Buah</td>
             <td>Puskesmas</td>
-            <td>Baik</td>
-            <td>2020</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
             <td>5000000</td>
-            <td></td>
-            <td></td>
+            <td>5000000</td>
+            <td>Pembelian</td>
             <td>2020-01-01</td>
+            <td>Baik</td>
             <td>-</td>
         </tr>
     </tbody>
